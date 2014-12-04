60bfae8f60720d001594cfbffcc9e8b7b452eda7
Romain <romain.gazielly@gmail.com>
2014-12-04 01:36:24 +0100

    Réglages Home

--- scripts/controllers/home.js
+++ scripts/controllers/home.js
@@ -1,28 +1,34 @@
 app.controller('HomeController', function ($scope) {
-	var perso = $(".liste-persos li")
+	var perso = $(".liste-persos li");
 	perso.hover(over, out);
 
 	function over(){
-		// Changement dynamique du titre
-		var title = $(this).find('img').attr('alt');
-		$('#title-persos').html(title);
+		var $this = $(this);
+		var persoId = $this.attr('id');
+		
+		if(persoId != 'moz-unknow'){
 
-		// Changement dynamique du fond du petit personnage
-		perso.removeClass('current');
-		$(this).addClass('current');
+			// Changement dynamique du titre
+			var title = $this.find('img').attr('alt');
+			$('#title-persos').html(title);
 
-		// Changement dynamique du fond du gros personnage
-		var bigPerso = $(this).attr('id').substr(4);
-		$('.preview').attr('id',bigPerso);
+			// Changement dynamique du fond du petit personnage
+			perso.removeClass('current');
+			$this.addClass('current');
 
-		// Changement dynamique du gros personnage
-		$('.preview img').attr('src', 'img/persos/big-'+bigPerso+'.png');
-		$('.preview img').attr('alt', title);
+			// Changement dynamique du fond du gros personnage
+			var bigPerso = $this.attr('id').substr(4);
+			$('.preview').attr('id',bigPerso);
+
+			// Changement dynamique du gros personnage
+			$('.preview img').attr('src', 'img/persos/big-'+bigPerso+'.png');
+			$('.preview img').attr('alt', title);
+		}
 
 	}
 
 	function out(){
-		// TweenMax.to(this, 0.5, {backgroundColor:"blue"})
+		// TweenMax.to($(this), 0.5, {backgroundColor:"blue"})
 		// TweenMax.to($(this).find("span"), 0.3, {rotation:0, scale:1, x:0, overwrite:"all"})
 	}
 })
--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -7,25 +7,36 @@ app.controller('MainController', function() {
 });
 
 function animMenu() {
+	var contactOpen = false;
 	var tl = new TimelineLite({paused:true});
 	var overlay = $('.overlay');
 	var subMenu = $('.subheader');
 	var content = $('.content');
 	var contact = $('#contact');
+	var close = $('#subhead-close');
 
 	tl.to(overlay, 0, {css:{display:'block'}});
 	tl.to(overlay, .5, {css:{opacity:1}}, "+=0");
-	tl.to(subMenu, .5, {css:{top:103}, ease:Power4.easeInOut}, "-=.5");
-	tl.to(content, .5, {css:{marginTop:155}}, "-=.5");
+	tl.to(subMenu, .2, {css:{top:103}, ease:Power4.easeInOut}, "-=.5");
+	tl.to(content, .2, {css:{marginTop:155, ease:Power4.easeInOut}}, "-=.5");
 	
 	contact.on('click', function(e){
-		e.preventDefault();
-		contact.addClass('current');
-		tl.play()
+		if(!contactOpen){
+			e.preventDefault();
+			contact.addClass('current');
+			tl.play();
+			contactOpen = true;
+		}else{
+			closeMenu();
+		}
 	});
 
-	$('#subhead-close').on('click', function(e){
+	close.on('click', closeMenu);
+
+	function closeMenu(){
 		tl.reverse();
 		contact.removeClass('current');
-	});
+		contactOpen = false;
+	}
 }
+
